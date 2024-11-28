b4481c9c0972f70005ee734e15a2230a5491e97f
CSK <73425927+cr2007@users.noreply.github.com>
2024-11-28 14:30:41 +0400

    Removes hardcoded file path and unused import

--- java/src/main/java/Day01_Trebuchet/Day1.java
+++ java/src/main/java/Day01_Trebuchet/Day1.java
@@ -3,12 +3,10 @@ package Day01_Trebuchet;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Day1 {
     public static void main(String[] args) {
-        // String filePath = "input.txt";
-        String filePath = "C:\\Users\\shiva\\Documents\\GitHub Repos\\Advent-Of-Code-2023\\java\\src\\main\\java\\Day01_Trebuchet\\input.txt";
+        String filePath = "input.txt";
 
         List<Integer> numbers = new ArrayList<>();
         int sum = 0;
@@ -21,19 +19,13 @@ public class Day1 {
                 if (digits.length > 0) {
                     int number = Integer.parseInt(digits[0] + digits[digits.length - 1]);
                     numbers.add(number);
-                } else {
-                    break;
-                }
-
-                // System.out.println(line.trim());
+                } else break;
             }
         } catch (IOException e) {
             System.out.println("File not found");
         }
 
-        for (int number : numbers) {
-            sum += number;
-        }
+        for (int number : numbers) sum += number;
 
         System.out.println("Sum of numbers: " + sum);
     }
